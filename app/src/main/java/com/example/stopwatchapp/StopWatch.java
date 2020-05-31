b7eb0c1cd21df682cc9063cc4bd2a3c703794b3e
Abanoub <abanoubshowky2018@gmail.com>
2020-05-31 18:03:17 +0200

    added animation

--- app/src/main/java/com/example/stopwatchapp/StopWatch.java
+++ app/src/main/java/com/example/stopwatchapp/StopWatch.java
@@ -3,15 +3,20 @@ package com.example.stopwatchapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageView;
 
 public class StopWatch extends AppCompatActivity {
     Button startButton;
+    Button stopButton;
     ImageView iconImageView;
     Animation animation;
+    Chronometer timerHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,35 @@ public class StopWatch extends AppCompatActivity {
         setContentView(R.layout.activity_stop_watch);
         startButton = (Button)findViewById(R.id.startButton);
         iconImageView = (ImageView)findViewById(R.id.icanchor);
+        stopButton = (Button)findViewById(R.id.stoptButton);
+        timerHere = (Chronometer)findViewById(R.id.timerHere);
 
-        animation = AnimationUtils.loadAnimation(this,R.anim.)
+        stopButton.setAlpha(0);
+        timerHere.setAlpha(0);
+
+        animation = AnimationUtils.loadAnimation(this,R.anim.roundingalone);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iconImageView.startAnimation(animation);
+                stopButton.animate().alpha(1).setDuration(300).start();
+                startButton.animate().alpha(0).setDuration(300).start();
+//                start timer here
+                timerHere.animate().alpha(1).setDuration(200).start();
+                timerHere.setBase(SystemClock.elapsedRealtime());
+                timerHere.start();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iconImageView.clearAnimation();
+                stopButton.animate().alpha(0).setDuration(300).start();
+                startButton.animate().alpha(1).setDuration(300).start();
+                timerHere.animate().alpha(0).setDuration(200).start();
+            }
+        });
     }
 }
